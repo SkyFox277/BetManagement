15154e840695f344754172fb45e80a89f9f1f143
Johann Friesen <jfriesen@isb-bonn.de>
2014-10-07 16:45:52 +0200

    initialcommit

---
+++ SOAPServer/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JF
+ * Date: 07.10.2014
+ * Time: 16:21
+ */ 
---
+++ WebClient/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ISB
+ * Date: 07.10.2014
+ * Time: 16:22
+ */ 

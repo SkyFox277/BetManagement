a85b47e01e860ad6547119809d24f9b35ed8b826
Sebastian Scholz <Sebastian.Scholz@isb-bonn.de>
2014-10-08 12:34:19 +0200

    Kommende TODOS in index implemntiert.

--- SOAPServer/index.php
+++ SOAPServer/index.php
@@ -5,3 +5,8 @@
  * Date: 07.10.2014
  * Time: 16:21
  */
+//TODO 1 Rausfinden wie das Framework zu nutzen ist.
+//TODO 2 Rausfinden wie automatisch beim Speichern in HTDOCS kopiert wird.
+//TODO 3 Grundstruktur aufbauen.
+//TODO 4 Controller für die Benutzer implementieren.
+//TODO 5 Methode zum Annehmen von Benutzernalagen implementieren.

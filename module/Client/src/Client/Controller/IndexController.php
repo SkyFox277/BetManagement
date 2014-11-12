798968b73f712293c221c4dcee45c303917efa44
i <i@sky-lap>
2014-11-12 07:53:46 +0100

    Application in Client umbenannt

--- module/Client/src/Client/Controller/IndexController.php
+++ module/Client/src/Client/Controller/IndexController.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Application\Controller;
+namespace Client\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Soap\Client;

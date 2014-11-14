29668c0f81f4422eb8f4e8741b755234f06d4270
SkyFox277 <skyfox277@gmail.com>
2014-11-14 16:55:01 +0100

    Update serverconfig.local.php

--- config/autoload/serverconfig.local.php
+++ config/autoload/serverconfig.local.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ * 
+ * 
+ * !!!!ALLE .local.* Datein aus dem commit herausnehmen!!!!
+ * dies machst du mit r-click auf Datei/Team/Advanced/Assume unchanged
+ *
+ */
+ 
+ 
 return array(
     'ServerConfig'  =>  array(
         'uri'       =>  'http://xxxx/services',

80699eaa15624499451f2e84305735d08f9b1673
SkyFox277 <i@sky-lap>
2014-11-26 14:49:47 +0100

    TODOs

--- module/Client/src/Client/Controller/ClientController.php
+++ module/Client/src/Client/Controller/ClientController.php
@@ -6,7 +6,6 @@
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 namespace Client\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
@@ -15,156 +14,162 @@ use Zend\View\Model\ViewModel;
 use Services\Model\Group;
 use Zend\Form\Annotation\Object;
 
-
 class ClientController extends AbstractActionController
-{  
-    const ROUTE_LOGIN        = 'client/login';
-    const ROUTE_REGISTER     = 'client/register';
-    
-    const CONTROLLER_NAME    = 'zfcuser';
-    
+{
+
+    const ROUTE_LOGIN = 'client/login';
+
+    const ROUTE_REGISTER = 'client/register';
+
+    const CONTROLLER_NAME = 'zfcuser';
+
     /**
+     *
      * @var UserService
      */
     protected $userService;
-    
+
     /**
+     *
      * @var Form
      */
     protected $loginForm;
-    
+
     /**
+     *
      * @var Form
      */
     protected $registerForm;
-    
+
     /**
+     *
      * @todo Make this dynamic / translation-friendly
      * @var string
      */
     protected $failedLoginMessage = 'Authentication failed. Please try again.';
-    
+
     /**
+     *
      * @var UserControllerOptionsInterface
      */
     protected $options;
     
-    //     public function __construct($userService, $options, $registerForm, $loginForm)
-    //     {
-    //         $this->userService = $userService;
-    //         $this->options = $options;
-    //         $this->registerForm = $registerForm;
-    //         $this->loginForm = $loginForm;
-    //     }
+    // public function __construct($userService, $options, $registerForm, $loginForm)
+    // {
+    // $this->userService = $userService;
+    // $this->options = $options;
+    // $this->registerForm = $registerForm;
+    // $this->loginForm = $loginForm;
+    // }
     /**
-     * Einstiegsfunktion des Soap-Clients. (non-PHPdoc)
+     * Einstiegsfunktion des Soap-Clients.
+     * (non-PHPdoc)
+     * 
      * @see \Zend\Mvc\Controller\AbstractActionController::indexAction()
      */
     public function indexAction()
     {
         $sconfig = $this->getServiceLocator()->get('Config')['ServerConfig'];
-        $options = array('compression'  => SOAP_COMPRESSION_ACCEPT,
-                        'cache_wsdl'    => 0,
-                        'soap_version'  => SOAP_1_2,
+        $options = array(
+            'compression' => SOAP_COMPRESSION_ACCEPT,
+            'cache_wsdl' => 0,
+            'soap_version' => SOAP_1_2
         );
         
+        $client = new Client($sconfig['wsdl'], $options);
         
-        echo "client </br>";
-     	$client = new Client($sconfig['wsdl'], $options);
-
-     	
-
-     	$ns   = "Services\API";
-     	
-     	$auth = new \stdClass();
-     	$auth->username = 'fake_user';
+        // TODO das 2. Password soll in der SESSION gespeichert werden und über den SOAP Header abgefragt werden
+        // Dieser 2. password soll automatisch für die aktuelle session generiert und in der DB beim Anmelden gespeichert werden
+        
+        // TODO Ablauf:
+        /**
+         * auf die Seite gekommen egal wohin : => Wilkommen, Login
+         * Register: Daten speichern => Login
+         * Eingeloggt: => Übersichtseite
+         * .....
+         */
+        
+        // TODO DB User Spalten: HeaderPassword, Eingeloggt_bis
+        
+        $auth = new \stdClass();
+        $auth->username = 'fake_user';
         $auth->password = 'fake_pass';
-
-        $auth_vals    = new \SoapVar($auth, SOAP_ENC_OBJECT);
-
-        //The 2nd variable, 'authenticate' is a method that exists inside of the SOAP service (you must create it, see next example)
-        $authenticate = new \SoapHeader($ns,'authenticate',array($auth), true);
-
+        $auth_vals = new \SoapVar($auth, SOAP_ENC_OBJECT);
+        $authenticate = new \SoapHeader($sconfig['location'], 'authenticate', array(
+            $auth
+        ), true);
         
         $client->addSoapInputHeader($authenticate, false);
-        
-        
         
         try {
             $response = $client->hello();
             
             echo $response;
-        }
-        catch(\SoapFault $e){
+        } catch (\SoapFault $e) {
             
-            if ($e->getMessage() == 401){
+            if ($e->getMessage() == 401) {
                 echo "falsche LogIn Daten!!!";
             }
-
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             echo "Es ist ein fehler auf der Webseite aufgetreten.";
         }
         
-//              	        echo "<pre>";
-//              	        var_dump($client->getLastRequest());
-//              	        echo "</pre>";
-             	        
-//              	        echo "<pre>";
-//              	        var_dump($client->getLastRequestHeaders());
-//              	        echo "</pre>";
-        
-//              	        echo "<pre>";
-//              	        var_dump($client->getLastResponse());
-//              	        echo "</pre>";
-             	        
-//              	        echo "<pre>";
-//              	        var_dump($client->getLastResponseHeaders());
-//              	        echo "</pre>";
-        //?????????????????????????
+        // ?????????????????????????
         /**
          * _preProcessArguments
-         * addSoapInputHeader
+         *
          * setUserAgent
          */
         
+//         $res = $client->authenticate($auth_vals);
         
-//      	$res = $client->authenticate($auth_vals);
-     	
-//      	        echo "<pre>";
-//      	        var_dump($res);
-//      	        echo "</pre>";
-     	
-     	echo "<br>----------TESTS------------";
-//      	echo  $client->getClassmap();
-               
-//         echo "<br>" .$client->signin();
-                
-        // Tests------------------------
-        echo "<br>";
-        //$result = $client->getGTable();
-                
-        echo "<br>----------------------";
-        echo "<br>";
-//         echo $client->getLastRequest();
-        echo "<br>----------------------";
-        echo "<br>"; 
-//         echo $client->getLastResponse();
-        echo "<br>----------------------";
+//         echo "<pre>";
+//         var_dump($res);
+//         echo "</pre>";
         
-
+//         echo "<br>----------TESTS------------";
+        
+//         echo $client->getClassmap();
+        
+//         echo "<br>" . $client->signin();
+        
+//         echo "<pre>";
+//         var_dump($client->getLastRequest());
+//         echo "</pre>";
+        
+//         echo "<br>----------------------";
+//         echo "<br>";
+        
+//         echo "<pre>";
+//         var_dump($client->getLastRequestHeaders());
+//         echo "</pre>";
+        
+//         echo "<br>----------------------";
+//         echo "<br>";
+        
+//         echo "<pre>";
+//         var_dump($client->getLastResponse());
+//         echo "</pre>";
+        
+//         echo "<br>----------------------";
+//         echo "<br>";
+        
+//         echo "<pre>";
+//         var_dump($client->getLastResponseHeaders());
+//         echo "</pre>";
+        
+//         echo "<br>----------------------";
+        
         return new ViewModel();
     }
-    
-    
-public function loginAction()
+
+    public function loginAction()
     {
         echo "test1 </br>";
-               
+        
         return new ViewModel();
     }
-    
-    
+
     public function registerAction()
     {
         // if the user is logged in, we don't need to register
@@ -173,47 +178,48 @@ public function loginAction()
             return $this->redirect()->toRoute($this->options->getLoginRedirectRoute());
         }
         // if registration is disabled
-        if (!$this->options->getEnableRegistration()) {
-            return array('enableRegistration' => false);
+        if (! $this->options->getEnableRegistration()) {
+            return array(
+                'enableRegistration' => false
+            );
         }
-
+        
         $request = $this->getRequest();
         $service = $this->userService;
         $form = $this->registerForm;
-
+        
         if ($this->options->getUseRedirectParameterIfPresent() && $request->getQuery()->get('redirect')) {
             $redirect = $request->getQuery()->get('redirect');
         } else {
             $redirect = false;
         }
-
-        $redirectUrl = $this->url()->fromRoute(static::ROUTE_REGISTER)
-            . ($redirect ? '?redirect=' . rawurlencode($redirect) : '');
+        
+        $redirectUrl = $this->url()->fromRoute(static::ROUTE_REGISTER) . ($redirect ? '?redirect=' . rawurlencode($redirect) : '');
         $prg = $this->prg($redirectUrl, true);
-
+        
         if ($prg instanceof Response) {
             return $prg;
         } elseif ($prg === false) {
             return array(
                 'registerForm' => $form,
                 'enableRegistration' => $this->options->getEnableRegistration(),
-                'redirect' => $redirect,
+                'redirect' => $redirect
             );
         }
-
+        
         $post = $prg;
         $user = $service->register($post);
-
+        
         $redirect = isset($prg['redirect']) ? $prg['redirect'] : null;
-
-        if (!$user) {
+        
+        if (! $user) {
             return array(
                 'registerForm' => $form,
                 'enableRegistration' => $this->options->getEnableRegistration(),
-                'redirect' => $redirect,
+                'redirect' => $redirect
             );
         }
-
+        
         if ($service->getOptions()->getLoginAfterRegistration()) {
             $identityFields = $service->getOptions()->getAuthIdentityFields();
             if (in_array('email', $identityFields)) {
@@ -223,10 +229,13 @@ public function loginAction()
             }
             $post['credential'] = $post['password'];
             $request->setPost(new Parameters($post));
-            return $this->forward()->dispatch(static::CONTROLLER_NAME, array('action' => 'authenticate'));
+            return $this->forward()->dispatch(static::CONTROLLER_NAME, array(
+                'action' => 'authenticate'
+            ));
         }
-
+        
         // TODO: Add the redirect parameter here...
-        return $this->redirect()->toUrl($this->url()->fromRoute(static::ROUTE_LOGIN) . ($redirect ? '?redirect='. rawurlencode($redirect) : ''));
+        return $this->redirect()->toUrl($this->url()
+            ->fromRoute(static::ROUTE_LOGIN) . ($redirect ? '?redirect=' . rawurlencode($redirect) : ''));
     }
 }
